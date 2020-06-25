b0f5d7b90b8d7eedf32267ce4f822b454ef7c3cf
Mateusz Kowalik <mateusz.kowalik91@outlook.com>
2020-06-25 19:40:35 +0200

    Add findFunctions

---
+++ TestProject/Katalog.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestProject
+{
+    class Katalog
+    {
+        private string _thematicDepartmant;
+        public List<Position> Positions;
+        public Katalog()
+        {
+            Positions = new List<Position>();
+        }
+        public Katalog(string thematicDepartmant)
+        {
+            this._thematicDepartmant = thematicDepartmant;
+        }
+        public void AddPosition(Position position)
+        {
+            Positions.Add(position);
+        }
+        public void DisplayAllPositions()
+        {
+            foreach (var item in Positions)
+            {
+                item.DisplayInfo();
+            }
+        }
+        /// <summary>
+        /// Find position by id
+        /// </summary>
+        /// <param name="id"></param>
+        public List<Position> FindPosition(int id)
+        {
+            var findList = new List<Position>();
+            foreach (var item in Positions)
+            {
+                if (item.Id == id)
+                {
+                    findList.Add(item);
+                }
+            }
+            return findList;
+        }
+        public List<Position> FindPosition(string title)
+        {
+            var findList = new List<Position>();
+            foreach (var item in Positions)
+            {
+                if (item.Title == title)
+                {
+                    findList.Add(item);
+                }
+            }
+            return findList;
+        }
+    }
+}
