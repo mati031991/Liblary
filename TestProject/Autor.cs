e989efe447dcabc41a7cb94c59173e73e96ac781
Mateusz Kowalik <mateusz.kowalik91@outlook.com>
2020-06-25 15:15:18 +0200

    Add autor

---
+++ TestProject/Autor.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestProject
+{
+    public class Autor
+    {
+        private string _forname;
+        private string _name;
+
+        public Autor()
+        {
+
+        }
+        public Autor(string forname, string name)
+        {
+            _forname = forname;
+            _name = name;
+        }
+    }
+}
