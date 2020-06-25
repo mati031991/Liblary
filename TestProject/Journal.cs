34cf1b0c982a99279d473f947a6d4a38fdc77da8
Mateusz Kowalik <mateusz.kowalik91@outlook.com>
2020-06-25 18:30:10 +0200

    Add Journal

--- TestProject/Journal.cs
+++ TestProject/Journal.cs
@@ -4,7 +4,28 @@ using System.Text;
 
 namespace TestProject
 {
-    public class Journal
+    public class Journal : Position
     {
+        private int _number;
+
+        public Journal()
+        {
+
+        }
+        public Journal(string title, int id, string publisher, int publisherYear, int number)
+        {
+            this.Title = title;
+            this.Id = id;
+            this.Publisher = publisher;
+            this.PublisherYear = publisherYear;
+            this._number = number;
+        }
+
+        public override void DisplayInfo()
+        {
+            Console.WriteLine(
+                 "Tytuł: {0}, Id: {1}, Wydawnictwo: {2}, Rok wydania: {3}, Numer: {4} ", this.Title, this.Id, this.Publisher, this.PublisherYear, this._number
+                 );
+        }
     }
 }
