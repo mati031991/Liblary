860a8711ccb1eaaaa68a461f9426b7a94dda8c43
Mateusz Kowalik <mateusz.kowalik91@outlook.com>
2020-06-25 15:28:44 +0200

    Add positions abstract class

---
+++ TestProject/Position.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestProject
+{
+    public abstract class Position
+    {
+        public string Title { get; set; }
+
+        public int Id { get; set; }
+
+        string Publisher { get; set; }
+
+        int PublisherYear { get; set; }
+
+        public Position()
+        {
+
+        }
+        public Position(string title, int id, string publisher, string publisherYear)
+        {
+            this.Title = title;
+            this.Id = id;
+            this.Publisher = publisher;
+            this.PublisherYear = PublisherYear;
+        }
+        public abstract void DisplayInfo();
+    }
+}
